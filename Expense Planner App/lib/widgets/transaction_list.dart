0c530263589fa0c78bdad9876a3b6e2703a38098
Shrayan Bhattacharya <shrayanjob08@gmail.com>
2022-05-11 19:17:09 +0530

    Proceeded the App

--- Expense Planner App/lib/widgets/transaction_list.dart
+++ Expense Planner App/lib/widgets/transaction_list.dart
@@ -12,23 +12,25 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: <Widget>[
-              Text(
-                'No transactions added yet!',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  )),
-            ],
-          )
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
+              ],
+            );
+          })
         : ListView.builder(
             itemBuilder: (ctx, index) {
               return Card(
@@ -43,7 +45,7 @@ class TransactionList extends StatelessWidget {
                     child: Padding(
                       padding: EdgeInsets.all(6),
                       child: FittedBox(
-                        child: Text('\Rs.${transactions[index].amount}'),
+                        child: Text('\$${transactions[index].amount}'),
                       ),
                     ),
                   ),
